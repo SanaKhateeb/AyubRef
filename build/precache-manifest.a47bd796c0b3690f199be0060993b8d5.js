66de2c313eb620ca1c6e4d0b6567b6300f04894c
SanaK <sanakhateeb14@hotmail.com>
2024-01-09 13:03:48 -0700

    fix

---
+++ build/precache-manifest.a47bd796c0b3690f199be0060993b8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0fec7b744704be1fcaf1f174eefb06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af7b9d5dfa83712f1a5a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "af7b9d5dfa83712f1a5a",
+    "url": "/static/js/main.31fd1394.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);
