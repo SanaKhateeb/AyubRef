7e59f81d6df526b292cf2a3e9c555b60df2584e8
SanaK <sanakhateeb14@hotmail.com>
2024-01-08 21:47:36 -0700

    Updates

---
+++ precache-manifest.fb9b6da2f30bfa123d514868aa8807e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26451a77167a9056863049805192e573",
+    "url": "/AyubRef/index.html"
+  },
+  {
+    "revision": "af7b9d5dfa83712f1a5a",
+    "url": "/AyubRef/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/AyubRef/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/AyubRef/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "af7b9d5dfa83712f1a5a",
+    "url": "/AyubRef/static/js/main.31fd1394.chunk.js"
+  },
+  {
+    "revision": "cbdac76bd4197b6ecdaa",
+    "url": "/AyubRef/static/js/runtime~main.47695647.js"
+  }
+]);
